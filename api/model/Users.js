69c161cc8eaaca833d00195df572ccad199ebe99
Kamvelihle Sikalana <86585438+SmasherK@users.noreply.github.com>
2023-09-08 15:25:07 +0200

    Update Users.js

--- api/model/Users.js
+++ api/model/Users.js
@@ -5,7 +5,7 @@ const {tokenCreate} = require('../middleware/AuthenticateUser')
 class Users{
     getUsers(req,res){
         const query =`
-        SELECT userID, CONCAT(firstName,'',lastName) 'Fullname',Age,
+        SELECT userID, CONCAT(firstName,' ',lastName) 'Fullname',Age,
         Gender,userRole,emailAdd,userImg
         FROM Users
         `
@@ -19,7 +19,7 @@ class Users{
     }
     getUser(req,res){
         const query =`
-        SELECT userID, CONCAT(firstName,'',lastName) 'Fullname',Age,
+        SELECT userID, CONCAT(firstName,' ',lastName) 'Fullname',Age,
         Gender,userRole,emailAdd,userImg
         FROM Users
         WHERE userID = ${req.params.id}
