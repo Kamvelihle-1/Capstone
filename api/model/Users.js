18f8b6b16cd1c2d6c20b09217aab8e855d24413d
Kamvelihle Sikalana <86585438+SmasherK@users.noreply.github.com>
2023-09-11 11:23:51 +0200

    Update Users.js

--- api/model/Users.js
+++ api/model/Users.js
@@ -112,6 +112,7 @@ class Users{
                         res.json({
                             status:res.statusCode,
                             token,
+                            result[0],
                             msg:"You are loged in"
                         })
                     } else {
