e635b21945ff12a3413dec544a149f9c6d0d2e4b
Kamvelihle Sikalana <86585438+SmasherK@users.noreply.github.com>
2023-09-11 07:57:44 +0200

    Update index.js

--- api/controller/index.js
+++ api/controller/index.js
@@ -53,7 +53,7 @@ routes.patch('/user/:id/cart/:id',bodyParser.json(),(req,res)=>{
 routes.delete('/user/:id/cart',(req,res)=>{
     carts.deleteCart(req,res)
 })
-routes.delete('/user/:id/cart/:id',(req,res)=>{
+routes.delete('/user/:id1/cart/:id2',(req,res)=>{
     carts.deleteCartItem(req,res)
 })
 routes.post('/user/:id/cart',bodyParser.json(),(req,res)=>{
