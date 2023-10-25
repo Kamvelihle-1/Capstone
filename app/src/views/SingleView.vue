925f59395d5a0ed0f562527f84f24bf1ea5aa9bd
Kamvelihle Sikalana <86585438+Kamvelihle-1@users.noreply.github.com>
2023-10-25 09:27:08 +0200

    Update SingleView.vue

--- app/src/views/SingleView.vue
+++ app/src/views/SingleView.vue
@@ -81,7 +81,7 @@ export default {
   methods:{
     addToCart(){
       console.log(this.result !=" ");
-      if (this.result?.length > 0) {
+      if (this.result?.length > 0 || this.result !=" ") {
         this.payload.userID = this.result?.userID
         this.data.prodQuantity =this.qCount
         this.payload.data = this.data
